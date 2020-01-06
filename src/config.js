57ec0d0083c5b9d63714d4774711c3a5903ee707
Jeff Martin <jmartin@onetechnologies.net>
2020-01-06 13:59:20 -0600

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "jmartin-notes-app-upload"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "YOURhttps://w01mr1rnhe.execute-api.us-west-2.amazonaws.com/prod "
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_tV6NG4yZl",
+    APP_CLIENT_ID: "6uotl9nmsg0dpeefjqqvaj5378",
+    IDENTITY_POOL_ID: "us-west-2:2d020cc4-55bf-4e9d-8381-fa7449317ed2"
+  }
+};
